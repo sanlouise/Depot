37b354300f2559369aa7bca8fe7d2aabd76eb150
sandralouise <sandra.adamshallie@gmail.com>
2016-01-25 12:47:28 +0000

    Add cart to product listing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,26 @@
 class Product < ActiveRecord::Base
-  validates :title, :description, :image_url, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
-  validates :image_url, allow_blank: true, format: {with: %r{\.(gif|jpg|png|)\Z}i,
-            message: "This must be a JPG, GIF or PNG image."
+
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_items
+
+  validates :description, :image_url, :title, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true, length: { minimum: 10 }
+
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|jpeg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
   }
-  
-  def self.latest
-    Product.order(:updated_at).last
+
+  private
+
+  def ensure_not_referenced_by_any_line_items
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, "Line Items present")
+      return false
+    end
   end
 end
